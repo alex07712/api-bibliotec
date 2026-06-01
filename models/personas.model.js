c25f38c8dc8024a1c64abc3ad0b647ff71a6152d
alex07712 <103295553+alex07712@users.noreply.github.com>
2026-06-01 01:03:12 -0600

    Change password hashing to use callbacks
    Refactor password hashing to use callbacks instead of async/await.

--- models/personas.model.js
+++ models/personas.model.js
@@ -12,14 +12,19 @@ const PersonaSchema = mongoose.Schema({
 }, { timestamps: true });
 
 // Hashear contraseña antes de guardar
-PersonaSchema.pre('save', async function() {
-  if (!this.isModified('password')) return;
+PersonaSchema.pre('save', function(next) {
+  if (!this.isModified('password')) return next();
   
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  // Si hay error, la promesa se rechaza automáticamente y Mongoose lo maneja
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+      this.password = hash;
+      next();
+    });
+  });
 });
-
 //compara contraseña
 PersonaSchema.methods.comparePassword = async function(candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
