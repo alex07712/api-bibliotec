bc2a5e30d8895281f3c01980c37bc190a8d8b725
alex07712 <103295553+alex07712@users.noreply.github.com>
2026-06-01 01:21:07 -0600

    Update personas.model.js

--- models/personas.model.js
+++ models/personas.model.js
@@ -13,7 +13,6 @@ const PersonaSchema = mongoose.Schema({
 
 PersonaSchema.pre('save', async function() {
   if (!this.isModified('password')) return;
-  
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
