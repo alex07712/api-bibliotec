c00b2747b80e458459fbba2d8dc194ed4ea41c06
Angel Alexander Cano <cano2006castillo@gmail.com>
2026-05-31 21:11:16 -0600

    error de credenciales

--- models/personas.model.js
+++ models/personas.model.js
@@ -12,8 +12,10 @@ const PersonaSchema = mongoose.Schema({
 }, { timestamps: true });
 
 //hashear password antes de guardar 
-PersonaSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+PersonaSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
   try {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
